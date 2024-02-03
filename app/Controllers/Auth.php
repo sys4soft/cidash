43c682bd7d0afd715c1bc6cbee074faa34dbd570
João Ribeiro <business@sys4soft.com>
2024-02-03 17:12:34 +0000

    master

---
+++ app/Controllers/Auth.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Auth extends BaseController
+{
+    public function login()
+    {
+        echo 'login';
+    }
+}

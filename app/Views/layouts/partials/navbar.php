26c542315fb699d0f3e6aa215e4ae659285fa722
João Ribeiro <business@sys4soft.com>
2024-02-11 16:17:24 +0000

    006

--- app/Views/layouts/partials/navbar.php
+++ app/Views/layouts/partials/navbar.php
@@ -17,7 +17,7 @@
             <div class="dropdown-menu dropdown-menu-end bg-secondary border-0 rounded-0 rounded-bottom m-0">
                 <a href="#" class="dropdown-item">
                     <div class="d-flex align-items-center">
-                        <img class="rounded-circle" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
+                        <img class="rounded-circle" src="<?= base_url('assets/images/user.jpg') ?>" alt="" style="width: 40px; height: 40px;">
                         <div class="ms-2">
                             <h6 class="fw-normal mb-0">Jhon send you a message</h6>
                             <small>15 minutes ago</small>
@@ -27,7 +27,7 @@
                 <hr class="dropdown-divider">
                 <a href="#" class="dropdown-item">
                     <div class="d-flex align-items-center">
-                        <img class="rounded-circle" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
+                        <img class="rounded-circle" src="<?= base_url('assets/images/user.jpg') ?>" alt="" style="width: 40px; height: 40px;">
                         <div class="ms-2">
                             <h6 class="fw-normal mb-0">Jhon send you a message</h6>
                             <small>15 minutes ago</small>
@@ -37,7 +37,7 @@
                 <hr class="dropdown-divider">
                 <a href="#" class="dropdown-item">
                     <div class="d-flex align-items-center">
-                        <img class="rounded-circle" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
+                        <img class="rounded-circle" src="<?= base_url('assets/images/user.jpg') ?>" alt="" style="width: 40px; height: 40px;">
                         <div class="ms-2">
                             <h6 class="fw-normal mb-0">Jhon send you a message</h6>
                             <small>15 minutes ago</small>
@@ -74,7 +74,7 @@
         </div>
         <div class="nav-item dropdown">
             <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                <img class="rounded-circle me-lg-2" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
+                <img class="rounded-circle me-lg-2" src="<?= base_url('assets/images/user.jpg') ?>" alt="" style="width: 40px; height: 40px;">
                 <span class="d-none d-lg-inline-flex">John Doe</span>
             </a>
             <div class="dropdown-menu dropdown-menu-end bg-secondary border-0 rounded-0 rounded-bottom m-0">
--- app/Views/main/home.php
+++ app/Views/main/home.php
@@ -1,6 +1,49 @@
 <?= $this->extend('layouts/layout_main') ?>
 <?= $this->section('content') ?>
 
-<h3>TESTE</h3>
+<div class="container-fluid pt-4 px-4">
+    <div class="row g-4">
+        <div class="col">
+            <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
+                TESTE
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid pt-4 px-4">
+    <div class="row g-4">
+        <div class="col">
+            <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
+                TESTE
+            </div>
+        </div>
+        <div class="col">
+            <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
+                TESTE
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid pt-4 px-4">
+    <div class="row g-4">
+        <div class="col">
+            <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
+                TESTE
+            </div>
+        </div>
+        <div class="col">
+            <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
+                TESTE
+            </div>
+        </div>
+        <div class="col">
+            <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
+                TESTE
+            </div>
+        </div>
+    </div>
+</div>
 
 <?= $this->endSection() ?>
